f3bb125668c4d26d7669553a0ad404b0c3d43b2e
jeff <jmcgirr@stevens.edu>
2016-02-24 19:58:06 -0500

    basic html/css stuff

--- app/app.js
+++ app/app.js
@@ -2,4 +2,16 @@
  * Created by jeffm on 2/24/2016.
  */
 
-var app = angular.module('main',[]);
+
+
+(function(){
+    var app = angular.module('main',[]);
+
+    app.controller('MainController',function(){
+        this.btn = callBtn;
+    });
+
+    var callBtn = {
+        name: 'Take a Call'
+    }
+});
