9ae8cea99dc29adfe7562646ff3fcfee2afd2650
Austin <austin.cawley@gmail.com>
2016-03-01 18:49:22 -0500

    Adding some templates for a location as well as starting with the rest service

--- app/shared/location/location.js
+++ app/shared/location/location.js
@@ -17,22 +17,38 @@
             });
     }]);
 
-    app.controller('LocationController', function() {
-
-    });
-
     app.directive('locationCard', function() {
        return {
            restrict: 'E',
-           templateUrl: 'locationCard.html'
-       }
+           templateUrl: 'app/shared/location/location-card.html',
+           controller: function() {
+               this.tab = 0;
+
+               this.isShowing = function(tabIndex) {
+                   return tabIndex === this.tab;
+               };
+           },
+           controllerAs: "card"
+       };
     });
 
-    app.directive('locationRatings', function() {
+    app.directive('locationRating', function() {
+
+
         return {
             restrict: 'E',
-            templateUrl: 'locationRating.html'
-        }
+            templateUrl: 'app/shared/location/location-rating.html',
+            controller: ['$scope', function($scope) {
+                $scope.averageRating = function(ratings) {
+                    var sum = 0;
+                    for (var i = 0; i < ratings.length; i++) {
+                        sum += ratings[i].value;
+                    }
+
+                    return ratings.length != 0 ? sum/ratings.length : 0;
+                };
+            }]
+        };
     });
 
 })();
